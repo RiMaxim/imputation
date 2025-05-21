eef79fb0c93d95f3a343cf33d83563914152fdc2
RI MAXIM <maxim.ree@googlemail.com>
2025-05-21 18:33:28 +0200

    Create 6_sorted_bam_to_gvcf.sh

---
+++ 6_sorted_bam_to_gvcf.sh
@@ -0,0 +1,5 @@
+java -jar /home/MaxRi/scripts/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar AddOrReplaceReadGroups -I $1.bam -O $1.gr.bam -ID group1 -SM $1 -PL Illumina -LB lib1 -PU unit1
+java -jar /home/MaxRi/scripts/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar MarkDuplicates -I $1.gr.bam -O $1.gr.dup.bam -M $1.dup.txt --REMOVE_DUPLICATES true --CREATE_INDEX true
+rm $1.bam $1.gr.bam
+
+java -jar /home/MaxRi/scripts/gatk-4.6.1.0/gatk-package-4.6.1.0-local.jar HaplotypeCallerSpark -R ./reference/Ssal_v3.1_genomic.fna -I $1.gr.dup.bam -O $1.g.vcf.gz -ERC GVCF
